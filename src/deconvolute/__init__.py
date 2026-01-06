e2f209834eedc5b738a7d13fb9434d0628e6667f
David <54682095+daved01@users.noreply.github.com>
2026-01-06 18:28:34 +0100

    ci: add summary step (#3)

--- src/deconvolute/__init__.py
+++ src/deconvolute/__init__.py
@@ -2,7 +2,7 @@ from .canary.engine import Canary
 from .core.results import CanaryResult, DetectionResult
 from .exceptions import DeconvoluteError, SecurityDetectedError
 
-__version__ = "0.1.0a2"
+__version__ = "0.1.0a1"
 
 __all__ = [
     "Canary",
