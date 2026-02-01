d06dd9cd141843a29389cf429da10835b6a3388d
daved01 <54682095+daved01@users.noreply.github.com>
2026-02-01 10:32:30 +0100

    feat: improve model imports

--- src/deconvolute/__init__.py
+++ src/deconvolute/__init__.py
@@ -1,7 +1,7 @@
 from .core.orchestrator import a_scan, guard, scan
 from .detectors.base import DetectionResult
-from .detectors.content import LanguageDetector
-from .detectors.integrity import CanaryDetector
+from .detectors.content import LanguageDetector, LanguageResult, SignatureDetector
+from .detectors.integrity import CanaryDetector, CanaryResult
 from .errors import DeconvoluteError, ThreatDetectedError
 
 __version__ = "0.1.0a8"
@@ -11,8 +11,11 @@ __all__ = [
     "scan",
     "a_scan",
     "CanaryDetector",
+    "CanaryResult",
     "DetectionResult",
     "LanguageDetector",
+    "LanguageResult",
+    "SignatureDetector",
     "ThreatDetectedError",
     "DeconvoluteError",
 ]
